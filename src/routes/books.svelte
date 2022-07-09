438dd327b98b82d2126d232eaaef41f7db5c1b6a
citingdude <glenn.reumers@hotmail.com>
2022-07-09 09:11:33 +0200

    Create books page

---
+++ src/routes/books.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  // your script goes here
+</script>
+
+<main>
+  <section>
+    <div class="container">
+      <h1 class="text-3xl font-bold underline">Books</h1>
+    </div>
+  </section>
+</main>
+
