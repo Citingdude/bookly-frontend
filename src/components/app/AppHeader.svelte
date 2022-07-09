9ebdf852249d250c95315109502d17deae470eb5
citingdude <glenn.reumers@hotmail.com>
2022-07-09 09:11:16 +0200

    Create header

---
+++ src/components/app/AppHeader.svelte
@@ -0,0 +1,12 @@
+<header class="py-4 shadow-2xl shadow-stone-700/10">
+  <div class="container flex justify-between">
+    <a class="font-bold text-2xl text-stone-900" href="/">Bookly</a>
+
+    <nav class="flex space-x-4">
+      <a class="text-lg font-medium" href="/">Home</a>
+      <a class="text-lg font-medium" href="/books">Books</a>
+    </nav>
+
+    <p class="text-lg font-medium">Logout</p>
+  </div>
+</header>
